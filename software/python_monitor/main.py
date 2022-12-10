90ee9bc86efa9c46bde989c69de463a399a1ec78
seadniksic <57331248+seadniksic@users.noreply.github.com>
2022-12-09 22:33:21 -0500

    added mediapipe face detection solution using tflite model still running on PC.  Next step is to load model onto teensy (need extra ram to come in so we can actually buffer a singular image from arducam)

--- software/python_monitor/main.py
+++ software/python_monitor/main.py
@@ -6,13 +6,20 @@ import time
 import cv2
 import signal
 import matplotlib.pyplot as plt
+import mediapipe as mp
 from PIL import ImageFile
 import numpy as np
 import sys
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 ser = serial.Serial('COM3', 3000000, timeout=None)
 img = bytearray(b'')
+img_width = 320
+img_height = 240
 faceCascade = cv2.CascadeClassifier("assets\\haarcascade.xml")
+FACE_DETECTION = 0
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
 def main():
     # make sure the 'COM#' is set according the Windows Device Manager
@@ -34,6 +41,7 @@ def main():
         now = time.time()
         ser.write(b'\x01')
         try:
+            # get_img()
             im.set_data(get_img())
         except:
             continue
@@ -60,44 +68,57 @@ def get_img():
             break
 
     now = time.time()
-    # I hate this because the time for message to be sent varies and this can crash the program
-    # time.sleep(.08) #wait for image to be fully sent over serial bus
-                    #avg image ~3500 bytes which can be sent at 4.6ms (apparently take more time, idk)
-                    #using 10 here to be safe
     
     img = ser.read_until(b"\n\n\n", 8000)
-    # print(img)
     img = img[:-3]
-    # while byte != b'':
-    #     byte = ser.read()
-    #     # print(byte)
-    #     img.extend(byte)
-    # print(ser.readline())
-        # print(img)
     
     print(sys.getsizeof(img))
-    # print(f"One img trans: {time.time() - now}")
+
     image_np = np.frombuffer(img, np.uint8)
-    # print(image_np)
+
     imageBGR = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    imageRGB = cv2.cvtColor(imageBGR, cv2.COLOR_BGR2RGB)
 
-# image = cv2.cvtColor(imageBGR , cv2.COLOR_BGR2GRAY)
+    #media pipe processing
+    if FACE_DETECTION == 0:
 
-    faces = faceCascade.detectMultiScale(
-        cv2.cvtColor(imageBGR , cv2.COLOR_BGR2GRAY),
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
+        with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detection:
 
-    imageRGB = cv2.cvtColor(imageBGR , cv2.COLOR_BGR2RGB)
+            results = face_detection.process(imageRGB)
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(imageRGB, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # Draw face detections of each face.
+            if not results.detections:
+                return imageRGB
+
+            # annotated_image = imageBGR.copy()
+            for detection in results.detections:
+                # Draw only bounding box
+                # coords = detection.location_data.relative_bounding_box
+                # coords = normalize_coords(coords)
+                # cv2.rectangle(imageRGB, coords[0], coords[1], (255, 255, 255), 2)
+
+                #Draw bounding box and landmarks
+                mp_drawing.draw_detection(imageRGB, detection)
+
+    elif FACE_DETECTION == 1:
+
+        faces = faceCascade.detectMultiScale(
+            cv2.cvtColor(imageBGR , cv2.COLOR_BGR2GRAY),
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(imageRGB, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     return imageRGB
 
 # print()
+
+def normalize_coords(coords):
+
+    return (int(coords.xmin * img_width), int(coords.ymin * img_height)), (int(coords.xmin * img_width+coords.width * img_width), int(coords.ymin*img_height+coords.height*img_height))
 
 if __name__ == "__main__":
     main()
@@ -114,3 +135,55 @@ if __name__ == "__main__":
 #         print(num)
 
 # ser.close()
+
+# import cv2
+
+
+# # For static images:
+# IMAGE_FILES = []
+# with mp_face_detection.FaceDetection(
+#     model_selection=1, min_detection_confidence=0.5) as face_detection:
+#   for idx, file in enumerate(IMAGE_FILES):
+#     image = cv2.imread(file)
+#     # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+#     results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+#     # Draw face detections of each face.
+#     if not results.detections:
+#       continue
+#     annotated_image = image.copy()
+#     for detection in results.detections:
+#       print('Nose tip:')
+#       print(mp_face_detection.get_key_point(
+#           detection, mp_face_detection.FaceKeyPoint.NOSE_TIP))
+#       mp_drawing.draw_detection(annotated_image, detection)
+#     cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+
+# # For webcam input:
+# cap = cv2.VideoCapture(0)
+# with mp_face_detection.FaceDetection(
+#     model_selection=0, min_detection_confidence=0.5) as face_detection:
+#   while cap.isOpened():
+#     success, image = cap.read()
+#     if not success:
+#       print("Ignoring empty camera frame.")
+#       # If loading a video, use 'break' instead of 'continue'.
+#       continue
+
+#     # To improve performance, optionally mark the image as not writeable to
+#     # pass by reference.
+#     image.flags.writeable = False
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     results = face_detection.process(image)
+
+#     # Draw the face detection annotations on the image.
+#     image.flags.writeable = True
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#     if results.detections:
+#       for detection in results.detections:
+#         mp_drawing.draw_detection(image, detection)
+#     # Flip the image horizontally for a selfie-view display.
+#     cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
+#     if cv2.waitKey(5) & 0xFF == 27:
+#       break
+# cap.release()
